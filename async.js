4a3ca0627e962b03a8f3f3eeffb98d46a0b58bca
taeshik Hwang <ghjkghj@naver.com>
2017-06-29 10:12:41 +0900

    Call Async func in a promise chain

--- async.js
+++ async.js
@@ -13,9 +13,11 @@ const fetch = require('node-fetch');
 async function showGitHubUser(handle) {
   const url = `https://api.github.com/users/${handle}`;
   const response = await fetch(url);
-  const user = await response.json();
-  console.log(user.name);
-  console.log(user.location);
+  return await response.json();
 }
 
-showGitHubUser('mariusschulz');
+showGitHubUser('mariusschulz')
+  .then(user => {
+    console.log(user.name);
+    console.log(user.location);
+  });
