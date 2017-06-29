2dca6a21857510321380059767c0e7922ab22250
taeshik Hwang <ghjkghj@naver.com>
2017-06-29 10:31:18 +0900

    Convert any func into an async func

--- async.js
+++ async.js
@@ -10,14 +10,19 @@ const fetch = require('node-fetch');
 //     });
 // }
 
-async function showGitHubUser(handle) {
-  const url = `https://api.github.com/users/${handle}`;
-  const response = await fetch(url);
-  return await response.json();
+class GitHubApiClient {
+  async fetchUser(handle) {
+    const url = `https://api.github.com/users/${handle}`;
+    const response = await fetch(url);
+    return await response.json();
+  }
 }
 
-showGitHubUser('mariusschulz')
-  .then(user => {
-    console.log(user.name);
-    console.log(user.location);
-  });
+(async () => {
+  const client = new GitHubApiClient();
+  const user = await client.fetchUser('mariusschulz');
+  console.log(user.name);
+  console.log(user.location);
+})();
+
+
