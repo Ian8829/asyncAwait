42b45010b4ffe1f121358000aa376d4ec432b869
taeshik Hwang <ghjkghj@naver.com>
2017-06-29 10:40:41 +0900

    Handle errors in async func

--- async.js
+++ async.js
@@ -1,28 +1,24 @@
 const fetch = require('node-fetch');
 
-// function showGitHubUser(handle) {
-//   const url = `https://api.github.com/users/${handle}`;
-//   fetch(url)
-//     .then(response => response.json())
-//     .then(user => {
-//       console.log(user.name);
-//       console.log(user.location);
-//     });
-// }
+async function fetchGitHubUser(handle) {
+  const url = `https://api.github.com/users/${handle}`;
+  const response = await fetch(url);
+  const body = await response.json();
 
-class GitHubApiClient {
-  async fetchUser(handle) {
-    const url = `https://api.github.com/users/${handle}`;
-    const response = await fetch(url);
-    return await response.json();
+  if (response.status !== 200)
+    throw Error(body.message);
+
+  return body;
+}
+
+async function showGitHubUser(handle) {
+  try {
+    const user = await fetchGitHubUser(handle);
+    console.log(user.name);
+    console.log(user.location);
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
   }
 }
 
-(async () => {
-  const client = new GitHubApiClient();
-  const user = await client.fetchUser('mariusschulz');
-  console.log(user.name);
-  console.log(user.location);
-})();
-
-
+showGitHubUser("mariusschulz");
